bf4f40fa86a9c04db128ab4200da2ae96ca78813
Naveen <naveenkrishnawodeyar@gmail.com>
2025-01-18 11:17:14 +0530

    primary commit

---
+++ src/main/java/com/gowri/ExceptioTractantemApplication.java
@@ -0,0 +1,13 @@
+package com.gowri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptioTractantemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptioTractantemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowri/ExceptioTractantemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptioTractantemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
