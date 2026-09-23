822dcd016625e1cfe0a40069892cc770ccba1783
Naveen <naveenkrishnawodeyar@gmail.com>
2026-09-23 23:04:04 +0530

    chore: modify ArithmeticException program

--- src/main/java/com/gowri/arithmetic/ArithmeticExceptionDemo.java
+++ src/main/java/com/gowri/arithmetic/ArithmeticExceptionDemo.java
@@ -1,32 +1,27 @@
+package com.gowri.arithmetic;
+
 /**
  * @author NaveenWodeyar
  * @date 22-Sept-2026 11:17:37 pm
  */
-package com.gowri.arithmetic;
-
 public class ArithmeticExceptionDemo {
 
     public static void main(String[] args) {
-
-        System.out.println("Program started");
-
+    	
         try {
             int a = 10;
             int b = 0;
 
             System.out.println("Before division");
-
             int result = a / b;
-
             System.out.println("Result: " + result);
 
         } catch (ArithmeticException e) {
 
             System.out.println("ArithmeticException caught");
-            System.out.println("Message: " + e.getMessage());
+            System.out.println("Message: " + e);
 
         } finally {
-
             System.out.println("Finally block executed");
         }
 
